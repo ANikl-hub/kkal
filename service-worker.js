48bc5af989475e2f9a310a6ef7616dd8b644b838
ANikl-hub <nikolaev.artem1997@mail.ru>
2026-04-28 01:31:50 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,22 @@
-const CACHE_NAME = 'calories-pwa-v1';
+const CACHE_NAME = 'calorie-goal-v1';
 const urlsToCache = [
-    '.',
-    'index.html',
-    'manifest.json'
+  '/calorie-pwa/',
+  '/calorie-pwa/index.html',
+  '/calorie-pwa/manifest.json',
+  '/calorie-pwa/icon-192.png',
+  '/calorie-pwa/icon-512.png'
 ];
 
-self.addEventListener('install', event=>{
-    event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', event=>{
-    event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
